1ea7b7073c55ac460eb6f97fe285a25838ab1c81
HouzuoGuo <guohouzuo@gmail.com>
2022-02-09 07:24:42 +0000

    Fix an off-by-one error in supervisor stuck threshold.

--- include/supervisor.h
+++ include/supervisor.h
@@ -6,7 +6,7 @@
 #define SUPERVISOR_FREE_MEM_RESET_THRESHOLD_KB 2
 // SUPERVISOR_STUCK_PROGRESS_THRESHOLD is the max number of consecutive readings the supervisor may observe from a critical parameter before it reboots.
 // It must be sufficiently high in order to accomodate the sleeping periods of individual tasks, e.g. the sleeping period of GPS.
-#define SUPERVISOR_STUCK_PROGRESS_THRESHOLD ((POWER_GPS_RUN_SLEEP_INTERVAL_SEC + LORAWAN_TX_KINDS * POWER_SLOWEST_TX_INTERVAL_SEC) * 1000 / SUPERVISOR_TASK_LOOP_DELAY_MS + 1)
+#define SUPERVISOR_STUCK_PROGRESS_THRESHOLD ((POWER_GPS_RUN_SLEEP_INTERVAL_SEC + LORAWAN_TX_KINDS * POWER_SLOWEST_TX_INTERVAL_SEC) * 1000 / SUPERVISOR_TASK_LOOP_DELAY_MS + 2)
 
 void supervisor_setup();
 void supervisor_reset();
