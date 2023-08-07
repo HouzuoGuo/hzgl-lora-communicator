d60fd96ccb95f8c7caa55eabd46b1e0d6a32bd2d
HouzuoGuo <guohouzuo@gmail.com>
2023-08-07 12:58:03 +0100

    gps: improve the reliability of saving GPS config

--- src/gps.cpp
+++ src/gps.cpp
@@ -44,16 +44,25 @@ void gps_on()
                 // Change serial output content type to NMEA.
                 ESP_LOGI(LOG_TAG, "demand NMEA output: %d", ublox.setUART1Output(COM_TYPE_NMEA));
                 // Disable unused NMEA sentences.
-                ublox.disableNMEAMessage(UBX_NMEA_GLL, COM_PORT_UART1);
-                ublox.disableNMEAMessage(UBX_NMEA_GSA, COM_PORT_UART1);
-                ublox.disableNMEAMessage(UBX_NMEA_GSV, COM_PORT_UART1);
-                ublox.disableNMEAMessage(UBX_NMEA_VTG, COM_PORT_UART1);
+                ESP_LOGI(LOG_TAG, "disable GLL: %d, disable GSA: %d, disable GSV: %d, disable VTG: %d",
+                         ublox.disableNMEAMessage(UBX_NMEA_GLL, COM_PORT_UART1),
+                         ublox.disableNMEAMessage(UBX_NMEA_GSA, COM_PORT_UART1),
+                         ublox.disableNMEAMessage(UBX_NMEA_GSV, COM_PORT_UART1),
+                         ublox.disableNMEAMessage(UBX_NMEA_VTG, COM_PORT_UART1));
                 // GGA - position fix, ZDA - date and time, RMC - position + course + speed.
                 ESP_LOGI(LOG_TAG, "enable GGA: %d, enable ZDA: %d, enable RMC: %d",
                          ublox.enableNMEAMessage(UBX_NMEA_GGA, COM_PORT_UART1),
                          ublox.enableNMEAMessage(UBX_NMEA_ZDA, COM_PORT_UART1),
                          ublox.enableNMEAMessage(UBX_NMEA_RMC, COM_PORT_UART1));
-                ESP_LOGI(LOG_TAG, "save config: %d", ublox.saveConfiguration());
+                for (int j = 0; j < 3; j++)
+                {
+                    bool save = ublox.saveConfiguration(5000);
+                    ESP_LOGI(LOG_TAG, "save config: %d", save);
+                    if (save)
+                    {
+                        break;
+                    }
+                }
                 break;
             }
             vTaskDelay(pdMS_TO_TICKS(100));
